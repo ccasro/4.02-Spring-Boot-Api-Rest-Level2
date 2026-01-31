3926de7591660bcc0818055245c40dfea4b07bc6
ccasro <ccasro@gmail.com>
2026-01-31 15:16:32 +0100

    feat: add Provider entity with unique name

---
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/model/Provider.java
@@ -0,0 +1,31 @@
+package cat.itacademy.s04.t02.n02.fruit.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "providers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Provider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String country;
+
+    public Provider(String name, String country){
+        this.name = name;
+        this.country = country;
+    }
+}
