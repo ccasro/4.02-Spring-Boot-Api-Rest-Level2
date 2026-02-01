905d88b51521bf2b7cf76594740183a64e7ef6a1
ccasro <ccasro@gmail.com>
2026-02-01 14:38:22 +0100

    feat: add provider update service

--- src/main/java/cat/itacademy/s04/t02/n02/fruit/repository/ProviderRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/repository/ProviderRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
     boolean existsByName(String name);
+
+    boolean existsByNameAndIdNot(String name, Long id);
 }
