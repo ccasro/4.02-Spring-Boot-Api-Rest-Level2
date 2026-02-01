5c500fca37a79af6afe28e77b2c88f27373b6f31
ccasro <ccasro@gmail.com>
2026-02-01 10:24:10 +0100

    feat: add repository and service support to filter fruits by provider

--- src/main/java/cat/itacademy/s04/t02/n02/fruit/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/repository/FruitRepository.java
@@ -4,6 +4,9 @@ import cat.itacademy.s04.t02.n02.fruit.model.Fruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
+    List<Fruit> findByProviderId(Long providerId);
 }
